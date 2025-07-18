f24c7462c7a00c65aae0bb3acfb039be6140b3c5
snehamanani07 <snehamanani36@gmail.com>
2025-07-19 01:23:28 +0530

    Update and Delete user

---
+++ Day 5/Mission/Mission.Entities/context/MissionDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mission.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Entities.context
+{
+    public class MissionDbContext : DbContext
+    {
+        public MissionDbContext(DbContextOptions<MissionDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegancyTimestampBehavior", true);
+            modelBuilder.Entity<User>().HasData(new User()
+            {
+                Id = 1,
+                FirstName = "Tatva",
+                LastName = "Admin",
+                EmailAddress = "admin@tatvasoft.com",
+                UserType = "admin",
+                Password = "Tatva@123",
+                PhoneNumber = "9876543210",
+                CreatedDate = new DateTime(2019, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
